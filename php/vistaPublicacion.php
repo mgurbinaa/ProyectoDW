3e0b9ef29a00532cb4c47630970a0f5328db516f
mikeurbi97 <aurbina24397@outlook.com>
2017-06-22 22:15:09 -0500

    Correcciones menores

--- php/vistaPublicacion.php
+++ php/vistaPublicacion.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+include('conexion.php');
+$conexion = conexion ();
+if(!isset($_SESSION['id_user'])){
+	header('location:login.php');
+}
+
+$id_user = $_SESSION['id_user'];
+$query  = "SELECT * FROM users where id_user = ".$id_user;
+$usuarioQuery = consulta($query);
+?>
 <html>
 	<head>
 		<title>Publicación | DEALS</title>
@@ -20,9 +32,23 @@
 			<div id="menu">
 				<ul>
 					<a href="index.php"><li class="actual">Hot</li></a>
-					<a href="login.php"><li>Iniciar sesion</li></a>
 					<a href="compartir.php"><li>compartir</li></a>
+					<a href="contactos.php"><li>contactanos</li></a>
 				</ul>
+			<table>
+				<?php 
+				while($rUsu = mysqli_fetch_array($usuarioQuery)):
+				?>
+				<tr>
+					<td align="left"><?php echo $rUsu['nombre'].' '.$rUsu['apellido']?></td>
+					<td align="left">
+					<a href="cerrar_sesion.php"> Finalizar sesión</a>
+					</td>
+				</tr>
+			</table>
+			<?php 
+			endwhile;
+			?>		
 			</div>
 			<div id="busqueda">
 				<input type="search" name="busqueda" placeholder="Buscar ...">
@@ -38,7 +64,7 @@
 
 			$datos = $db->getPubliData($_POST['idPubli']);
 			if($datos->num_rows > 0){
-				echo ""
+				echo "";
 			}
 
 			?>
