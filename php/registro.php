33a361c8ff7562ddae607323ce1b6135f72fe4f0
abigailre <abigailresmontes@outlook.com>
2017-06-22 21:25:28 -0500

    Update registro.php

--- php/registro.php
+++ php/registro.php
@@ -1,82 +1,67 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <title>Registro</title>
+<?php  
+$con = mysqli_connect('localhost','root','','appdesc');
+?>
+<html>
+<body>
+<title>Deals</title>
+   <link rel="stylesheet" type="text/css" href="estilos.css">
+   <meta charset="utf-8">
+   
+   <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700,900" rel="stylesheet">
 </head>
 <body>
-<div class="contenedor">
-<link href="../css/estilosLogin.css" rel="stylesheet">
-   <h1>Registro</h1>
-   <p>Introduce tus datos para poder ingresar</p>
-   <input type="text" id="usuario" placeholder="Nombre  o email">
-   <input type="text" id="Apellidos" placeholder="Apellidos">
-   <input type="password" id="password" placeholder="Password">
-   <button onclick="login()" id="boton">Iniciar Sesión</button>
-   <p>O si ya tienes cuenta, <a href="login.html">inicia sesión</a>.</p>
-   <p  id="msj-error"></p>
-
+   <div id="encabezado">
+      <div id="logo">
+         <div id="imagen">
+            <img src="img/logo.png">
+         </div>
+         <div id="nombre">
+            <h2>Deals</h2>
+            <h5>Las mejores Ofertas</h5>
+         </div>
+      </div>
+<div class="agregarNuevoUsuario">
    
-</div>
+      <link rel="stylesheet" type="text/css" href="estiloslogin.css">
+ <form method="post" action="registro.php">
+ <div class="contenedor">
+ <h1>Bienvenido</h1>
+ <p>Ingresa tus datos para crear una cuenta</p>
+         <input type="text" name="usuario" placeholder="Nombre de usuario" >
+         <br><br>
+         <input type="text" name="correo" placeholder="Correo Electrónico" >
+         <br><br>
+         <input type="text" name="nombre" placeholder="Nombre" >
+         <br><br>
+         <input type="text" name="apellido" placeholder="Apellido" >
+         <br><br>
+         <input type="password" name="password" placeholder="Password">
+         <br><br>
+         <input type="submit" name="registro" value="Registro">
+          <br><br>
+         <br><br>
+         <a href="login.html">  Inicia sesion </a>
+   </form>
+ </div>
 
-<script>
-   function login(){
-      //capturamos valores de login
-      usuario = document.getElementById('usuario').value;
-      password = document.getElementById('password').value;
-      //validar que tengan contenido
-      if (usuario != "" && password != "") {
 
-         //SI SON DIFERENTES DE ANADA, ENTRA EN EL LADO VERDADEDO
-         ajaxLogin = new XMLHttpRequest();
-         ajaxLogin.open('GET','validalogin.php?u=' +usuario+'&p='+password, true);
-         ajaxLogin.send();
-         ajaxLogin.onreadystatechange = function(){
-            if (ajaxLogin.readyState == 3 && ajaxLogin.status == 200) {
-               //ESTA CARGANDO
-               document.getElementById('usuario').disable = "true";
-               document.getElementById('password').disable = "true";
-               document.getElementById('boton').disable = "true";
-               document.getElementById('boton').innerHTML = "Cargando...";
-               document.getElementById('boton').style.opacity =".5";
-            }
-            if (ajaxLogin.readyState == 4 && ajaxLogin.status == 200) {
-               //YA HAY UNA RESPUESTA 
-               console.log(ajaxLogin.responseText)
-               if (ajaxLogin.responseText == "loguado") {
-                  //SE PUEDE LOGAR
-                  window.location.assign('http://www.google.com');
-               }
-               else{
-                  //NO INICIO DE SESION
-                  setTimeout(function(){
-                     document.getElementById('usuario').removeAttribute = ('disable');
-                     document.getElementById('password').removeAttribute = ('disable');
-                     document.getElementById('boton').removeAttribute = ('disable');
-                     document.getElementById('boton').innerHTML = "Iniciar sesion";
-                     document.getElementById('boton').style.opacity ="1";
-                     mensaje('tus datos no coinciden');
-                  }, 2000);
-                  
-               }
-            }
-         }
-      }
-      else{
-         //SI SON IGUALES A NADA, ENTRA EN EL LADO FALSO
-         mensaje('campos Incompletos');
-      }
-   }
+<?php 
+if (isset($_POST['registro'])) {
+   //CAPTURAR VARIABLES DEL POST
+   $usuario = $_POST['usuario'];
+   $correo = $_POST['correo'];
+   $nombre = $_POST['nombre'];
+   $apellido = $_POST['apellido'];
+   $password = $_POST['password'];
+   $password = MD5($password);
+  
 
-   function mensaje(msj){
-      cuadroMensaje = document.getElementById('msj-error');
-      cuadroMensaje.innerHTML = msj;
-      cuadroMensaje.classList.add('ver');
-      setTimeout(function(){
-         cuadroMensaje.classList.remove('ver')
-      }, 2000);
-   }
-</script>
-    
+   $insertarUsuario = "INSERT INTO users (usuario,correo,nombre,apellido,password) VALUES ('$usuario','$correo','$nombre','$apellido','$password')";
+   mysqli_query($con, $insertarUsuario);
+   echo "Te has Registrado correctamente!";
+}
+
+ ?>
+
 </body>
-</html>
+</html> 
