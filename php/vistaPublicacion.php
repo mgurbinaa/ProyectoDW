fffd3b817b874716dff2c40ee5f12e14ccb118df
Abigail <abigailresmontes@outlook.com>
2017-06-25 20:41:32 -0500

    Solo se cambiaron estilos

--- php/vistaPublicacion.php
+++ php/vistaPublicacion.php
@@ -16,6 +16,7 @@ $usuarioQuery = consulta($query);
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="../css/estilos.css">
 		<script type="text/javascript" src="../js/funciones.js"></script>
+		<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700,900" rel="stylesheet">
 	</head>
 	<body>
 		<div id="encabezado">
@@ -32,42 +33,51 @@ $usuarioQuery = consulta($query);
 			<div id="menu">
 				<ul>
 					<a href="index.php"><li class="actual">Hot</li></a>
-					<a href="compartir.php"><li>compartir</li></a>
+					<a href="crearPublicacion.php"><li>compartir</li></a>
 					<a href="contactos.php"><li>contactanos</li></a>
 				</ul>
-			<table>
-				<?php 
-				while($rUsu = mysqli_fetch_array($usuarioQuery)):
-				?>
-				<tr>
-					<td align="left"><?php echo $rUsu['nombre'].' '.$rUsu['apellido']?></td>
-					<td align="left">
-					<a href="cerrar_sesion.php"> Finalizar sesión</a>
-					</td>
-				</tr>
-			</table>
-			<?php 
-			endwhile;
-			?>		
-			</div>
-			<div id="busqueda">
-				<input type="search" name="busqueda" placeholder="Buscar ...">
-			</div>
-		</div>
 
+	<table>
+		<?php 
+		while($rUsu = mysqli_fetch_array($usuarioQuery)):
+			?>
+		<tr>
+		<div id=nombre>
+			<td>
+
+			<td align="left"><?php echo $rUsu['nombre'].' '.$rUsu['apellido']?></td>	
+			<a href="cerrar_sesion.php"> Finalizar sesión</a>
+			</div>
+			</td>
+		</tr>
+		</table>
+		<?php 
+		endwhile;
+		?>
+	
+		</div>
+		<div id="busqueda">
+		<input type="search" name="busqueda" placeholder="Buscar ...">
+	</div>
+	</div>
+	
+		<link rel="stylesheet" type="text/css" href="../css/estilosVista.css">
 		<div id="datosPublicacion">
+		
 			<div id='datosPubli'>
 				<h1 id='titulPubli'>TÍTULO</h1>
+				<img src='{$datos['imgPubli']}' id='imgPubli'>
 				<form action='{$datos['link']}' id='oferta'>
-					<button id='precioPubli' disabled>$ PRECIO</button>
-					<button id='linkPubli'>LINK A LA OFERTA</button>
+				<button id='precioPubli' disabled>$ PRECIO</button>
+				<button id='linkPubli'>LINK A LA OFERTA</button>
 				</form>
-					<img src='{$datos['imgPubli']}' id='imgPubli'>
+
 					<div id='califPubli'>
 						<p id='calif'>CALIFICACIÓN</p>
 					</div>
 					<p id='descPubli'>Descripción: </p>
 				</div>
+
 				<div id='comentarios'>
 					<h2 id='noComments'>No hay comentarios aún :(</h2>
 				</div>
